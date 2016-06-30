1c2476890656b34637ee6e7bdcb9c245ae80cc4b
Robin Lehrmann <role@vkf-renzel.de>
2016-06-30 12:37:51 +0200

    implent filter logic

--- Classes/RA/SilexRestApi/Filter/Builder/RequestFilterBuilder.php
+++ Classes/RA/SilexRestApi/Filter/Builder/RequestFilterBuilder.php
@@ -3,13 +3,23 @@
 namespace RA\SilexRestApi\Filter\Builder;
 
 use RA\SilexRestApi\Filter\Constraint\Equals;
+use RA\SilexRestApi\Filter\Constraint\GreaterThan;
+use RA\SilexRestApi\Filter\Constraint\LessThan;
+use RA\SilexRestApi\Filter\Constraint\LessThanOrEquals;
 use RA\SilexRestApi\Filter\Constraint\Like;
+use RA\SilexRestApi\Filter\Constraint\LogicalAnd;
 use RA\SilexRestApi\Filter\Constraint\NotEquals;
+use RA\SilexRestApi\Filter\Constraint\NotLike;
 use RA\SilexRestApi\Filter\Filter;
 use RA\SilexRestApi\Filter\FilterInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class RequestFilterBuilder implements BuilderInterface {
+
+    /**
+     * @var array
+     */
+    private $constraints = ['!', '<', '>', '=', '%'];
 
     /**
      * @var Request
@@ -21,7 +31,9 @@ class RequestFilterBuilder implements BuilderInterface {
     }
 
     /**
-     * @return FilterInterface
+     * @param FilterInterface|null $default
+     *
+     * @return Filter|FilterInterface
      */
     public function build(FilterInterface $default = NULL)
     {
@@ -33,11 +45,42 @@ class RequestFilterBuilder implements BuilderInterface {
 
         $filter->setRequest($this->request);
 
-        if ($this->request->get('parent') !== NULL) {
-            // TODO: Implement stuff here.
+        if ($this->request->get('filters')) {
+            foreach ($this->request->get('filters') as $field => $value) {
+                $condition = $this->getConditionByValue($value);
+                $value = $this->getCleanValue($value);
+                switch ($condition) {
+                    case '!':
+                            $fieldConstraint = new NotEquals($field, $value);
+                        break;
+                    case '<':
+                            $fieldConstraint = new LessThan($field, $value);
+                        break;
+                    case '<=':
+                            $fieldConstraint = new LessThanOrEquals($field, $value);
+                        break;
+                    case '>':
+                            $fieldConstraint = new GreaterThan($field, $value);
+                        break;
+                    case '>=':
+                            $fieldConstraint = new LessThanOrEquals($field, $value);
+                        break;
+                    case '%':
+                            $fieldConstraint = new Like($field, $value);
+                        break;
+                    case '!%':
+                            $fieldConstraint = new NotLike($field, $value);
+                        break;
+                    default:
+                            $fieldConstraint = new Equals($field, $value);
+                        break;
+                }
 
-            $constraint = new NotEquals('OXPARENTID', '');
-            $filter->setRootConstraint($constraint);
+                $constraint = new LogicalAnd();
+                $constraint->addConstraint($fieldConstraint);
+
+                $filter->setRootConstraint($constraint);
+            }
         }
 
         if ($this->request->get('itemsPerPage') !== NULL) {
@@ -55,5 +98,44 @@ class RequestFilterBuilder implements BuilderInterface {
         $filter->setPagination($itemsPerPage, $page);
 
         return $filter;
+    }
+
+    /**
+     * get clean value without any condition
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private function getCleanValue($value)
+    {
+        $start = 0;
+        foreach (str_split($value) as $char) {
+            if (in_array($char, $this->constraints)) {
+                $start++;
+            }
+        }
+
+        return substr($value, $start);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    private function getConditionByValue($value)
+    {
+        $constraint = '';
+
+        foreach (str_split($value) as $char) {
+            if (in_array($char, $this->constraints)) {
+                $constraint .= $char;
+            } elseif (ctype_alnum($char)) {
+                break;
+            }
+        }
+
+        return $constraint;
     }
 }
